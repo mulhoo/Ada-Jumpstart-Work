33bac0edbf8d1a19f8762e0af210050760ab1c4f
mulhoo <51768443+mulhoo@users.noreply.github.com>
2019-09-10 21:59:12 -0700

    Create NumberChallengev1.rb

---
+++ NumberChallengev1.rb
@@ -0,0 +1,6 @@
+puts "Welcome! Please provide #{3} numbers (no decimals): "
+first_number = gets.chomp.to_i
+second_number = gets.chomp.to_i
+third_number = gets.chomp.to_i
+
+puts "Adding #{20} to each of the numbers given to me gives values #{first_number + 20}, #{second_number + 20}, and #{third_number + 20}. Thank you for participating!"
